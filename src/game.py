7f907e909e7383c2a2b0bf7e19c087517ba17998
ƒkS124 <57041516+fkS124@users.noreply.github.com>
2022-04-24 12:32:57 +0200

    Performances and rules
    - Performance improvements in drawing (from pg.draw.polygon to pg.gfxdraw.filled_polygon)
    - Performance improvements in getting the number of neigbours
    - Press ENTER to go to the next gen

--- src/game.py
+++ src/game.py
@@ -3,6 +3,7 @@ from pygame import Vector2
 from pygame.gfxdraw import aapolygon
 from math import sqrt
 from .tile import Tile
+from copy import copy
 
 
 class Game:
@@ -19,11 +20,12 @@ class Game:
         # get the display
         self.screen = screen
         self.W, self.H = self.screen.get_size()
+        self.gen = 0
 
         # view settings
         self.d_pos = pg.Vector2(0, 0)
         self.scale = 1
-        self.size = 50
+        self.size = 10
 
         # all the tiles
         self.tiles_alive: list[Tile] = [Tile(self, (0, 0), self.size), Tile(self, (1, 1), self.size),
@@ -35,12 +37,12 @@ class Game:
                     self.neighbours.append(neighbour)
         self.dragging = False
 
-    def draw_hexagon(self, pos: tuple[int, int]):
+    def draw_hexagon(self, pos: tuple[int, int], color: tuple[int, int, int] = (0, 0, 0)):
         actual_pos = (pos[1] * self.size * self.scale * sqrt(3) / 2,
                       -(pos[0] + pos[1] * 0.5) * self.size * self.scale) + self.d_pos
         vertexes = [actual_pos + translation * self.scale * self.size for translation in Tile.vertexes_translation]
         vertexes.insert(0, actual_pos)
-        aapolygon(self.screen, vertexes, (0, 0, 0))
+        aapolygon(self.screen, vertexes, color)
 
     def handle_events(self, event):
         if event.type == pg.MOUSEBUTTONUP:
@@ -58,24 +60,30 @@ class Game:
             if self.dragging:  # drag the screen
                 self.d_pos += pg.Vector2(event.rel)
 
+        elif event.type == pg.KEYDOWN:
+            if event.key == 13:
+                self.next_generation()
+                self.update_neighbours()
+
     def update_neighbours(self):
         self.neighbours = []
+        pos_alive = [tile.pos for tile in self.tiles_alive]
+        pos_neighbour = []
         for tile in self.tiles_alive:
             for i in range(6):
-                if not (neighbour := Tile(self, (self.get_neighbours(tile.pos)[i]), self.size)) in (self.neighbours or self.tiles_alive):
-                    self.neighbours.append(neighbour)
-
+                new_neighbour = Tile(self, (self.get_neighbours(tile.pos)[i]), self.size)
+                if new_neighbour.pos not in pos_alive and new_neighbour.pos not in pos_neighbour:
+                    pos_neighbour.append(new_neighbour.pos)
+                    self.neighbours.append(new_neighbour)
 
     def number_of_neighbours(self, pos: tuple[int, int]):
-        n = 0
-        for i in range(6):
-            for tile in self.tiles_alive:
-                if self.get_neighbours(pos)[i] == tile.pos:
-                    n += 1
-        return n
+        all_tiles_pos = [tile.pos for tile in self.tiles_alive]
+        neighbours = self.get_neighbours(pos)
+        return sum([neighbours[i] in all_tiles_pos for i in range(6)])
 
     def next_generation(self):
 
+        current_generation = copy(self.tiles_alive)
         for tile in self.tiles_alive:
             for i in range(0, 6):
                 if self.number_of_neighbours(tile.pos) == 1:
@@ -91,6 +99,12 @@ class Game:
                 self.tiles_alive.append(tile)
                 T = True
 
+        for former_tile in current_generation:
+            self.tiles_alive.remove(former_tile)
+
+        self.gen += 1
+        print(len(self.tiles_alive), "generation :", self.gen)
+
     def draw_grid(self):
         drawn = []
         for neighbour in self.neighbours:
@@ -98,7 +112,7 @@ class Game:
                 drawn.append(neighbour.pos)
 
         for draw in drawn:
-            self.draw_hexagon(tuple(draw))
+            self.draw_hexagon(tuple(draw), color=(255, 0, 0))
 
     def get_neighbours(self, pos: tuple[int, int]):
         return [
@@ -112,9 +126,7 @@ class Game:
 
     def update(self):
         self.dragging = pg.mouse.get_pressed()[0]
-        self.draw_grid()
         for tile in self.tiles_alive:
             tile.draw(self.d_pos, self.scale)
-        self.next_generation()
-        self.update_neighbours()
-        print(self.tiles_alive)
+
+        self.draw_grid()
--- src/tile.py
+++ src/tile.py
@@ -1,6 +1,8 @@
 import pygame as pg
 from pygame import Vector2
+from pygame.gfxdraw import filled_polygon
 from math import sqrt
+from random import randint
 
 
 class Tile:
@@ -18,7 +20,6 @@ class Tile:
 
     ]
 
-
     def __init__(self, game_instance, pos: tuple[int, int], size: int):
 
         self.game_instance = game_instance
@@ -27,11 +28,13 @@ class Tile:
         self.size = size
         self.alive_list = [(0, 0)]
 
+        # self.COLOR = (randint(0, 255), randint(0, 255), randint(0, 255))
+
     def draw(self, d_pos, scale: int = 1):
         actual_pos = (self.pos[1]*self.size*sqrt(3)/2*scale, -(self.pos[0]+self.pos[1]*0.5)*self.size*scale) + d_pos
         vertexes = [actual_pos + translation * scale * self.size for translation in self.vertexes_translation]
         vertexes.insert(0, actual_pos)
-        pg.draw.polygon(self.game_instance.screen, self.COLOR, vertexes)  # draw the polygon
+        filled_polygon(self.game_instance.screen, vertexes, self.COLOR)  # draw the polygon
 
     def next_step(self) -> bool:
         all_tiles = self.game_instance.tiles
